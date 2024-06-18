f75d8ecc0e993a9b720f1e97222e6aef48232711
Konstantin Lakhmotkin <grimmjow@inbox.ru>
2024-06-18 22:20:39 +0300

    Initial commit

---
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology.services;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class VacationServiceTest {
+
+@Test
+    public void vacationMonths1() {
+    VacationService service = new VacationService();
+
+    int expected = 3;
+    int actual = service.calcVacationMonths(10_000, 3_000, 20_000);
+
+    Assertions.assertEquals(expected, actual);
+}
+
+    @Test
+    public void vacationMonths2() {
+    VacationService service = new VacationService();
+
+    int expected = 2;
+    int actual = service.calcVacationMonths(100_000, 60_000, 150_000);
+
+    Assertions.assertEquals(expected, actual);
+    }
+}
